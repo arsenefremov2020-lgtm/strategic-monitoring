6e11dd6babf52f4248aa312b02cd78dffc368874
arsenefremov2020-lgtm <arsen.efremov.2020@gmail.com>
2026-06-11 11:40:49 +0300

    Update 3_Оцінка_МіО_.py

--- pages/3_Оцінка_МіО_.py
+++ pages/3_Оцінка_МіО_.py
@@ -10,6 +10,11 @@ from supabase import create_client
 
 st.set_page_config(page_title="Оцінка МіО", layout="wide")
 
+st.logo(
+    "assets/Мінекономіки.png",
+    size="large"
+)
+
 FILE_PATH = "Під моніторинг СП.xlsx"
 SHEET_NAME = "Страт_матриця"
 
